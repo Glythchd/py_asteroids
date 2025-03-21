d15c41c7eb20700d42d6c678a92b54f487c05863
Glytchd <goodwin78ak@gmail.com>
2025-03-21 10:03:59 -0800

    ch2 l2

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
-print("Starting Asteroids!")
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
+def main():
+    pygame.init()
+    
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        # Check for quit events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  # This exits the main() function
+        
+        # Fill the screen with black
+        screen.fill("black")
+        
+        # Update the display
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
