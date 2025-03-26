1eb5632bf33af1b4d3a896c327db980ed038ecd9
Glytchd <goodwin78ak@gmail.com>
2025-03-26 08:14:22 -0800

    ch2 l5 complete

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    dt = 0
+
     while True:
         # Check for quit events
         for event in pygame.event.get():
@@ -18,9 +22,12 @@ def main():
         
         # Fill the screen with black
         screen.fill("black")
-        
+        # Draw the player
+        player.draw(screen)
         # Update the display
         pygame.display.flip()
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
